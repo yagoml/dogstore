07324ffbccb067732d682f21dbb9b64e2b8a5c24
yagodml <yagoskor@gmail.com>
2020-07-06 18:01:58 -0300

    project start

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import store from '@/store'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+Vue.use(VueRouter)
 
 new Vue({
+  store: store,
   render: h => h(App),
 }).$mount('#app')

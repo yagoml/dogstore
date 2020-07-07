e301f663e50446d13ee234b615d42482447a0982
yagodml <yagoskor@gmail.com>
2020-07-06 21:18:15 -0300

    store added

--- src/store.js
+++ src/store.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { listBreeds } from '@/services/dog-api'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    count: 0
+    breeds: {},
+    loading: false,
+    images: {}
   },
+
   mutations: {
-    increment (state) {
-      state.count++
+    setBreeds(state, breeds) {
+      state.breeds = breeds
+    },
+    setLoading(state, status) {
+      state.loading = status
+    },
+    setImage(state, params) {
+      state.images[params.breed] = params.image
+    }
+  },
+
+  actions: {
+    async getBreeds({commit}) {
+      commit('setLoading', true)
+      const breeds = await listBreeds()
+      commit('setBreeds', breeds)
+      commit('setLoading', false)
     }
   }
 })
